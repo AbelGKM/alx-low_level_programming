9b14e1240025ddbffe355608f259c8923dbc549a
abel getu <abelgetuk@gmail.com>
2022-09-19 07:35:05 -0700

    task 2-3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,22 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * _puts - a function that prints a string
- * @str: string input
- * Return: string
+ *_puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: returns no error
  */
+
 void _puts(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
+		putchar(*(str + i));
+		i++;
 	}
-	_putchar('\n');
+	putchar(10);
 }
